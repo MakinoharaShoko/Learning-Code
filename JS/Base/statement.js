7aecae0223d2757589faee1cb1bf2641ba48c3ef
MakinoharaShoko <salufthansa@foxmail.com>
2021-12-20 20:47:27 +0800

    Update statement.js

--- JS/Base/statement.js
+++ JS/Base/statement.js
@@ -16,3 +16,30 @@ const array1 = [1,2,3,4,5,6];
 for (const number of array1) {
     console.log(number);
 }
+
+//with 语句
+
+let location = {
+    country:'China',
+    province:'Anhui',
+    city:'He-fei'
+}
+
+with (location) {
+    console.log(country);
+    console.log(province);
+    console.log(city);
+}
+
+// switch
+
+const a = 0;
+
+switch (true){
+    case a<0:
+        console.log('a<0');
+        break;
+    case a>=0:
+        console.log('a>=0');
+        break;
+}//输出 a>=0
