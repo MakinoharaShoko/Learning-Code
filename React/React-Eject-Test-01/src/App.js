b15378f99fc909eb473f8863fedb775a092488bd
MakinoharaShoko <salufthansa@foxmail.com>
2021-12-24 00:53:52 +0800

    update to 1.0.6

--- React/React-Eject-Test-01/src/App.js
+++ React/React-Eject-Test-01/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import 'antd/dist/antd.css';
 import TestFly from './testFlyVar/testfly';
+import TestFly2 from './testFlyVar/testFly2';
 
 function App() {
     return (
         <div>
             <TestFly/> 
+            <TestFly2/>
         </div>
     );
 }
