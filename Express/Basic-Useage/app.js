3aae2865fb2c1b5d0e220210522ea8f8d7513e21
MakinoharaShoko <salufthansa@foxmail.com>
2022-01-02 21:18:56 +0800

    Update app.js

--- Express/Basic-Useage/app.js
+++ Express/Basic-Useage/app.js
@@ -11,14 +11,36 @@ const Port = 3001;
 app.use('/', express.static(__dirname + '/public'));//allow browser access resources
 process.env.PORT = Port;
 
-app.get('/test1',(req,res)=>{
+app.get('/test1', (req, res) => {
     res.send('123');
 })
 
-app.post('/testPost',(req,res)=>{
+app.post('/testPost', (req, res) => {
     let s = req.body['testData'];
     console.log(s);
     res.send(s);
 })
 
 app.listen(Port, () => console.log('服务器已就绪，运行在端口' + Port));
+
+const https = require('https')
+const options = {
+    hostname: 'ahuer.cn',
+    port: 443,
+    path: '/api/dialogBanner/all',
+    method: 'GET'
+}
+
+const req = https.request(options, res => {
+    console.log(`状态码: ${res.statusCode}`)
+
+    res.on('data', d => {
+        process.stdout.write(d)
+    })
+})
+
+req.on('error', error => {
+    console.error(error)
+})
+
+req.end()
