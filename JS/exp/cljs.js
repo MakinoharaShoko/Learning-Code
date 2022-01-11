25a188057fac5ede64d82a65f2dc632da1ac8c35
MakinoharaShoko <salufthansa@foxmail.com>
2022-01-11 14:57:51 +0800

    Update cljs.js

--- JS/exp/cljs.js
+++ JS/exp/cljs.js
@@ -1,4 +1,36 @@
 const cljs = require('cloudlogjs');
 
 const logger = new cljs();
+logger.init('https://cl.msfasr.com/','mongodb+srv://Kamome:Makinohara31415@ics-mongodb.kcslx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 logger.info('开始调试');
