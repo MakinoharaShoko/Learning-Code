a2607c45f48ad8ba4a136c2cde8d0bb7a509f657
Mahiru <makinoharashoko@outlook.com>
2022-02-22 22:34:03 +0800

    Update 3.js

--- Algorithm/LeetCode/3.js
+++ Algorithm/LeetCode/3.js
@@ -6,14 +6,16 @@ var lengthOfLongestSubstring = function (s) {
     let stringWindow = [];
     let maxLen = 0;
     for (let i = 0; i < s.length; i++) {
+        //如果窗口没有这个字符，那么加入这个字符
         if (!stringWindow.includes(s[i])) {
             stringWindow.push(s[i]);
         } else {
+            //先把之前与之重复的字符清除
             while (stringWindow.includes(s[i]))
                 stringWindow.shift();
-            stringWindow.push(s[i]);
+            stringWindow.push(s[i]);//最后再加上这个字符
         }
-        if (stringWindow.length > maxLen) {
+        if (stringWindow.length > maxLen) {//获取最大窗口
             maxLen = stringWindow.length;
         }
     }
