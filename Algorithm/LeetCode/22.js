64ea84fd9f0a5e7673874d48cf0d056f5a5d0c3e
Mahiru <makinoharashoko@outlook.com>
2022-03-03 19:30:06 +0800

    Update 22.js

--- Algorithm/LeetCode/22.js
+++ Algorithm/LeetCode/22.js
@@ -3,5 +3,27 @@
  * @return {string[]}
  */
 var generateParenthesis = function (n) {
+    const res = [];
+    let t = '';
+    //用递归
+    const gentrate = (n, i, t) => {
+        if (n === 0) {//左括号长度已满，补充右括号
+            for (let j = 0; j < i; j++) {
+                t = t + ')';
+            }
+            res.push(t)
+            return;
+        }
+        if (t.length === 0 || (t[t.length - 1] === ')' && n === i)) {//当字符串为空或括号闭合时，只补充左括号
+            gentrate(n - 1, i, t + '(');
+        } else {//补充左括号或有右括号
+            gentrate(n - 1, i, t + '(');
+            gentrate(n, i - 1, t + ')');
+        }
 
+    }
+    gentrate(n, n, t);
+    return res;
 };
+
+console.log(generateParenthesis(3));
