cfd6c114f4e710d8d06b8c6f63f08841ddd75a78
Mahiru <makinoharashoko@outlook.com>
2022-03-05 17:05:32 +0800

    Update 647.js

--- Algorithm/LeetCode/647.js
+++ Algorithm/LeetCode/647.js
@@ -3,5 +3,23 @@
  * @return {number}
  */
 var countSubstrings = function (s) {
-    
+    let res = 0;
+    for (let i = 0; i < s.length; i++) {
+        let delta = 0;
+        while (i - delta >= 0 && i + delta < s.length && s[i - delta] === s[i + delta]) {
+            res++;
+            delta++;
+        }
+        let l = 0, r = 0;
+        l = i - 1;
+        r = i;
+        while (l >= 0 && r < s.length && s[l] === s[r]) {
+            res++;
+            l--;
+            r++;
+        }
+    }
+    return res;
 };
+
+console.log(countSubstrings('aaa'));
