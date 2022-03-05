c769b43001d014db8b202c6534c07d3e5881ace8
Mahiru <makinoharashoko@outlook.com>
2022-03-05 16:42:15 +0800

    更新算法题

--- Algorithm/LeetCode/337.js
+++ Algorithm/LeetCode/337.js
@@ -11,6 +11,23 @@
  * @return {number}
  */
 var rob = function (root) {
-    //层序遍历，然后比较偷单层和偷双层哪个大
-    
+    let dp = [];
+    const dfs = (node) => {
+        if (!node) {
+            return [0, 0];
+        }
+        const left = dfs(node.left);
+        const right = dfs(node.right);
+
+        const current = [];
+        current[0] = Math.max(left[0], left[1]) + Math.max(right[0], right[1]);//这个不偷，左右选偷和不偷中大的。
+        current[1] = node.val + left[0] + right[0];//这个偷，左右不能偷
+        return current;
+    }
+    if (root) {
+        dp = dfs(root);
+    } else {
+        return 0;
+    }
+    return Math.max(dp[0], dp[1]);
 };
