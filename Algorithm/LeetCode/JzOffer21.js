19e6b026fdad8f17720c8f487d5e3cff5fee0b80
Mahiru <makinoharashoko@outlook.com>
2022-03-08 10:01:56 +0800

    Update JzOffer21.js

--- Algorithm/LeetCode/JzOffer21.js
+++ Algorithm/LeetCode/JzOffer21.js
@@ -2,6 +2,14 @@
  * @param {number[]} nums
  * @return {number[]}
  */
- var exchange = function(nums) {
-
+var exchange = function (nums) {
+    //选择所有的奇数和偶数
+    const a = [];
+    const b = [];
+    nums.forEach(e => {
+        if (e % 2 === 0) {
+            b.push(e);
+        } else a.push(e);
+    })
+    return a.concat(b);
 };
