21f142d593b0257c21a51805b58d6005d4e3e498
Mahiru <makinoharashoko@outlook.com>
2022-03-10 21:09:58 +0800

    Update JzOffer24.js

--- Algorithm/LeetCode/JzOffer24.js
+++ Algorithm/LeetCode/JzOffer24.js
@@ -10,13 +10,13 @@
  * @return {ListNode}
  */
 var reverseList = function (head) {
-    let cur = null;
-    let pre = head;
-    while(pre !==null){
-        const t = pre.next;
-        pre.next = cur;
-        cur = pre;
-        pre = t;
+    let next = null; //反转后的下一个
+    let curr = head;  //当前访问的
+    while(curr !==null){//当前访问的不是空
+        const t = curr.next; //当前的下一个（反转前）
+        curr.next = next; //改变下一个为反转后的下一个
+        next = curr; //反转后的下一个是当前的（也就是下一个curr的指向）
+        curr = t; //curr指向反转前的下一个
     }
-    return cur;
+    return next;
 };
