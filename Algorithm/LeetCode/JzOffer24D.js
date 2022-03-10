25047b232e993329ca37cd1e4aaf3f2959ea10c7
Mahiru <makinoharashoko@outlook.com>
2022-03-10 21:29:53 +0800

    Update JzOffer24D.js

--- Algorithm/LeetCode/JzOffer24D.js
+++ Algorithm/LeetCode/JzOffer24D.js
@@ -9,12 +9,13 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
-var reverseList = function (head) {
-    if (head === null || head.next === null) { //没有后继
+var reverseList = function (head) {//找反转后的头
+    if (head === null || head.next === null) { //没有后继，证明这是反转后的头
         return head;
     }
-    const ret = reverseList(head.next); //反转后的头
-    head.next.next = head;
-    head.next = null;
-    return ret;
+    const ret = reverseList(head.next); //递归找反转后的头
+    //此时当前的ret就是反转后的头
+    head.next.next = head;//回溯阶段：把每一个节点的下一个的下一个都改为自己
+    head.next = null;//回溯阶段：把自己的下一个改成null
+    return ret;//回溯结束，返回反转后的当前节点
 };
