42e957803de2c7d8855f0e9033a28c58904d9453
Mahiru <makinoharashoko@outlook.com>
2022-03-11 11:45:10 +0800

    Create JzOffer68-II.js

---
+++ Algorithm/LeetCode/JzOffer68-II.js
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+var lowestCommonAncestor = function (root, p, q) {
+    const findChild = (node) => {
+        if (!node)
+            return null;
+        if (node.val === p || node.val === q) {
+            return node;
+        }
+        let left = findChild(node.left);
+        let right = findChild(node.right);
+        if (left && right) {
+            return root;
+        }
+        if (left) {
+            return right;
+        }
+        if (right) {
+            return left;
+        }
+    }
+    return findChild(root);
+};
