e700df04c9b8cfdb3fd17f733a62035553a61263
MakinoharaShoko <makinoharashoko@outlook.com>
2022-04-04 20:37:48 +0800

    ignore target

--- Rust/world_hello/target/debug/world_hello.d
+++ Rust/world_hello/target/debug/world_hello.d
@@ -1,1 +1,1 @@
-c:\Users\Makinohara\Documents\GitHub\Learning-Code\Rust\world_hello\target\debug\world_hello.exe: c:\Users\Makinohara\Documents\GitHub\Learning-Code\Rust\world_hello\src\main.rs
+C:\Users\Admin\Documents\GitHub\Learning-Code\Rust\world_hello\target\debug\world_hello.exe: C:\Users\Admin\Documents\GitHub\Learning-Code\Rust\world_hello\src\main.rs
