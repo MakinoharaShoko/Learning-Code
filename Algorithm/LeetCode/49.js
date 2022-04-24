0bf8edbb6a8d6d3307091d16b348f2139c908334
Mahiru <makinoharashoko@outlook.com>
2022-04-24 15:42:16 +0800

    更新算法题

--- Algorithm/LeetCode/49.js
+++ Algorithm/LeetCode/49.js
@@ -1,7 +1,15 @@
 /**
+ * 将字符串分解为数组，并对数组排序,然后把排序结果相同的合并
+ * 返回 Map 的值
  * @param {string[]} strs
  * @return {string[][]}
  */
- var groupAnagrams = function(strs) {
-    
+var groupAnagrams = function (strs) {
+    const m = new Map();
+    strs.map(e => ({ raw: e, key: e.split('').sort().reduce((a, c) => a + c, '') }))
+        .forEach(e => m.set(e.key, m.has(e.key) ? [e.raw, ...m.get(e.key)] : [e.raw]));
+    return [...m].map(k => k[1]);
 };
+
+console.log(groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]));
+console.log('end');
