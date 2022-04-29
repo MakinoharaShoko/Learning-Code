5f54a308e90ff2047725da585e62b1f1c2a5fb60
Mahiru <Mahiru_@outlook.com>
2022-04-29 16:59:33 +0800

    Update test.ts

--- TS/test.ts
+++ TS/test.ts
@@ -2,12 +2,12 @@ interface Itest {
     name: string
     age: number
 }
-type Person2 = Pick<Itest, Exclude<keyof Itest, 'age'>> & { age?: number }
+type Person2 = Pick<Itest, Exclude<keyof Itest, 'age'>> & { age?: number };
 type Person3 = {
     [P in keyof Itest]?: Itest[P];
 }
 
-type makePropertyPartical<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>
+type MakePropertyPartical<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>;
 
 const obj: Person2 = { name: 'test' };
 const obj2 = { age: 1 };
