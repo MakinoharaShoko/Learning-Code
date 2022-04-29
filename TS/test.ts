5c42f12edafd04fe63410b5ba1caef1eb02e9215
Mahiru <Mahiru_@outlook.com>
2022-04-29 16:53:39 +0800

    Update test.ts

--- TS/test.ts
+++ TS/test.ts
@@ -1,2 +1,13 @@
-let l:number = 2;
-console.log(l);
+interface Itest {
+    name: string
+    age: number
+}
+type Person2 = Pick<Itest, Exclude<keyof Itest, 'age'>> & { age?: number }
+type Person3 = {
+    [P in keyof Itest]?: Itest[P];
+}
+
+type makePropertyPartical<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>
+
+const obj: Person2 = { name: 'test' };
+const obj2 = { age: 1 };
