105c0876e6f82efd5cff1a131a78da6c281d468b
Mahiru <makinoharashoko@outlook.com>
2022-05-04 21:31:29 +0800

    Create test-fetch-ahu.js

---
+++ JS/playground/test-fetch-ahu.js
@@ -0,0 +1,15 @@
+fetch("http://xsfw.ahu.edu.cn/xsfw/sys/xsqjapp/modules/leaveApply/editLeaveApply.do", {
+    "headers": {
+        "accept": "*/*",
+        "accept-language": "zh-CN,zh;q=0.9,ja;q=0.8,en;q=0.7",
+        "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+        "proxy-connection": "keep-alive",
+        "x-requested-with": "XMLHttpRequest"
+    },
+    "referrer": "http://xsfw.ahu.edu.cn/xsfw/sys/xsqjapp/*default/index.do",
+    "referrerPolicy": "strict-origin-when-cross-origin",
+    "body": "data=%7B%22SFLX_DISPLAY%22%3A%22%E6%98%AF%22%2C%22SFLX%22%3A%221%22%2C%22SFLKSZD_DISPLAY%22%3A%22%E5%90%A6%22%2C%22SFLKSZD%22%3A%220%22%2C%22LXQX_DISPLAY%22%3A%22%22%2C%22LXQX%22%3A%22%22%2C%22XXDZ%22%3A%22%22%2C%22JJLXR%22%3A%22%22%2C%22LXDH%22%3A%22%22%2C%22QJXZ_DISPLAY%22%3A%22%E5%9B%A0%E7%A7%81%E8%AF%B7%E5%81%87%22%2C%22QJXZ%22%3A%221%22%2C%22QJLX_DISPLAY%22%3A%22%E4%BA%8B%E5%81%87%22%2C%22QJLX%22%3A%224ce219b8eb7d4d7882b12bc6a1fb5155%22%2C%22SQSM%22%3A%22%22%2C%22QJKSRQ%22%3A%222022-05-05%22%2C%22QJJSRQ%22%3A%222022-05-05%22%2C%22QJTS%22%3A1%2C%22QJSY%22%3A%22%E8%8E%B7%E5%8F%96%E7%94%9F%E6%B4%BB%E5%BF%85%E9%9C%80%E5%93%81%22%2C%22ZMCL%22%3A%22%22%2C%22SJH%22%3A%2218226639319%22%2C%22SQBH%22%3A%228da5298c6e96439b9e4efe0eb7005a61%22%2C%22XSBH%22%3A%22E01914031%22%2C%22QJRQ%22%3A%222022-05-06%22%2C%22BEGINDATE%22%3A%222022-05-06%22%2C%22QJXLH%22%3A%228d6b2bf5b0f4497cb8389b372d8cd0fc%22%7D",
+    "method": "POST",
+    "mode": "cors",
+    "credentials": "include"
+});
