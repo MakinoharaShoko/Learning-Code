91c78c50edb852f1310b6054e3a5dd9edcb4285f
Mahiru <makinoharashoko@outlook.com>
2022-05-05 09:07:17 +0800

    doc(Leetcode42): add comments

--- Algorithm/LeetCode/42.js
+++ Algorithm/LeetCode/42.js
@@ -3,25 +3,25 @@
  * @return {number}
  */
 var trap = function (height) {
-    let s1 = 0;
-    let s2 = 0;
-    let sz = 0;
-    let max1 = 0;
-    let max2 = 0;
+    let s1 = 0; // 从左侧开始数，累加当前最高值
+    let s2 = 0; // 从右侧开始数，累加当前最高值
+    let sz = 0; // 柱子总面积
+    let max1 = 0; // 左侧开始的最大值
+    let max2 = 0; // 右侧开始的最大值
     height.map(e => {
         if (e > max1) {
             max1 = e;
         }
-        sz += e;
-        s1 += max1;
+        sz += e; // 累加柱子面积
+        s1 += max1; // 累加左面开始的面积
     })
     for (let i = height.length - 1; i >= 0; i--) {
         if (height[i] > max2) {
             max2 = height[i];
         }
-        s2 += max2;
+        s2 += max2; // 累加右面开始的面积
     }
-    const s3 = max1 * height.length;
+    const s3 = max1 * height.length; // 计算总面积
     return s1 + s2 - s3 - sz;
 };
 
