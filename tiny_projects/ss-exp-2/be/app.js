6fa0d71661e6382fb6af09ebfd886f113a933868
Mahiru <Mahiru_@outlook.com>
2022-05-09 13:58:24 +0800

    feat: add project ss-exp-2

---
+++ tiny_projects/ss-exp-2/be/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser')
+app.use(cors());
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+const Port = 3000;
+app.use('/', express.static(__dirname + '/public'));//allow browser access resources
+process.env.PORT = Port;
+
+app.get('/apitest', (req, res) => {
+    res.send('test OK !');
+})
+
+app.post('/apitest', (req, res) => {
+    let s = req.body['testData'];
+    console.log(s);
+    res.send(s);
+})
+
+app.listen(Port, () => console.log('服务器已就绪，运行在端口' + Port));
