1ba61f8e8aa50490f0f9b6103017ac8c3afc04c7
Mahiru <Mahiru_@outlook.com>
2022-05-30 15:03:10 +0800

    build c/s ss test

--- tiny-projects/ss-exp-2/be/init.js
+++ tiny-projects/ss-exp-2/be/init.js
@@ -13,7 +13,6 @@ MongoClient.connect(MongoUrl, (err, db) => {
         {Name: '张一', Math: 80, Chinese: 82, Chemical: 71, Physics: 87},
         {Name: '李二', Math: 86, Chinese: 90, Chemical: 91, Physics: 80},
         {Name: '王三', Math: 78, Chinese: 82, Chemical: 87, Physics: 76},
-        {Name: '赵四', Math: 75, Chinese: 85, Chemical: 80, Physics: 77},
     ]
     dbo.collection('data').insertMany(insertData, (err, result) => {
         if (err) throw err;
