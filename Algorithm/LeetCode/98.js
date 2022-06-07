2596d0727666327718d5a07637b5670d567cbb6b
Mahiru <makinoharashoko@outlook.com>
2022-06-07 15:18:39 +0800

    Update 98.js

--- Algorithm/LeetCode/98.js
+++ Algorithm/LeetCode/98.js
@@ -10,14 +10,17 @@
  * @param {TreeNode} root
  * @return {boolean}
  */
-var isValidBST = function (root) {
+ var isValidBST = function (root) {
     // 先假设是二叉搜索树
     let ret = true;
+    let go = true;
     // 递归判断，如果有一个节点不是，那就不是
     function judge(node, mustSmaller, mustBigger) {
-        if (node && node.val !== undefined) {
+        if (node && node.val !== undefined && go) {
             if (node.val >= mustSmaller || node.val <= mustBigger) {
                 ret = false;
+                go = false;
+                return;
             } else {
                 if (node.left) {
                     judge(node.left, node.val, mustBigger);
