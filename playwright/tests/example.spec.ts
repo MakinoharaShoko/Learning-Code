bdf252f497609a698f06bf24fd3d28a9613c66bd
Mahiru <Mahiru_@outlook.com>
2022-06-09 19:52:53 +0800

    feat: add a test case.

--- playwright/tests/example.spec.ts
+++ playwright/tests/example.spec.ts
@@ -34,3 +34,20 @@ test('baidu search', async ({page}) => {
   });
   await expect(isPass).toBe(true);
 });
+
+test('baidu search 2 ', async ({page}) => {
+  await page.goto('https://www.baidu.com/');
+  await page.locator('#kw').fill('机票');
+  await page.locator('#su').click();
+  await new Promise((resolve) => {
+    setTimeout(resolve, 1500);
+  })
+  const result = await page.locator('a');
+  const allText = await result.allInnerTexts();
+  let isPass = false;
+  allText.map(async e => {
+    if (e.match(/火车票/))
+      isPass = true;
+  });
+  await expect(isPass).toBe(true);
+});
