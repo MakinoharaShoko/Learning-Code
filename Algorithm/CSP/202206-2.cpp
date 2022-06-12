769ff3334ac9d8e62cbb848362cbeb81318bbca0
Mahiru <Mahiru_@outlook.com>
2022-06-12 15:37:59 +0800

    Create 202206-2.cpp

---
+++ Algorithm/CSP/202206-2.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int cbt[1001][1001];
+int lht[1001][2];
+
+int n,l,s;
+
+int main(){
+    cin>>n>>l>>s;
+    for(int i = 0;i<n;i++){
+        
+    }
+}
