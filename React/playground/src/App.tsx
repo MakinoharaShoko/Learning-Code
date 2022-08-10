2d5eb6520048c33543164cbf04e2aacb8a0c8221
Mahiru <Mahiru_@outlook.com>
2022-08-11 01:24:21 +0800

    Update App.tsx

--- React/playground/src/App.tsx
+++ React/playground/src/App.tsx
@@ -3,7 +3,7 @@ import {AnimationTest} from './pages/animationtest/AnimationTest'
 import WaterFall from "./pages/waterfallTest/WaterFall";
 
 function App() {
-  const list = [200, 300, 500, 100, 200, 300, 200, 700];
+  const list = [200, 300, 500, 100, 300, 300, 200, 700];
   return (
     <div className="App">
       <WaterFall list={list}/>
