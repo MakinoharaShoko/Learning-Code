d92383dd8374b734db4ce5bc45b75fe4b5568b04
Mahiru <Mahiru_@outlook.com>
2023-02-07 11:46:33 +0800

    Update ch06-map-fliter.hs

--- Haskell/Lesson/ch06/ch06-map-fliter.hs
+++ Haskell/Lesson/ch06/ch06-map-fliter.hs
@@ -10,3 +10,20 @@ filter' p (x:xs)
 
 t :: Integer
 t = sum (takeWhile (<10000) (filter odd (map (^2) [1..]))) -- 找所有小于 10000 的奇数的平方的和
+
+
+-- 取一个自然数，若为偶数就除以 2。 若为奇数就乘以 3 再加 1。 再用相同的方式处理所得的结果，得到一组数字构成的的链。
+chain :: (Integral a) => a -> [a]  
+chain 1 = [1]  
+chain n  
+    | even n =  n:chain (n `div` 2)  
+    | odd n  =  n:chain (n*3 + 1)
+
+
+-- 多少个链长度大于15
+numLongChains :: Int  
+numLongChains = length (filter isLong (map chain [1..100]))  
+    where isLong xs = length xs > 15
+
+listOfFuns :: [Integer -> Integer]
+listOfFuns = map (*) [0..]
