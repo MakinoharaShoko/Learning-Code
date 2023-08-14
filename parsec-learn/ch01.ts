69d6972b1f0593b38637ed3f0cf0223689717f1f
Mahiru <Mahiru_@outlook.com>
2023-08-14 22:33:09 +0800

    add

--- parsec-learn/ch01.ts
+++ parsec-learn/ch01.ts
@@ -1,6 +1,7 @@
 
 import { Token, buildLexer, expectEOF, expectSingleResult, list_sc, lrec_sc, rep, rule, tok } from 'typescript-parsec';
 import { alt, apply, kmid, opt, seq, str } from 'typescript-parsec';
+import { flattenDeep } from 'lodash'
 
 enum TokenKind {
   Space,
@@ -11,10 +12,12 @@ enum TokenKind {
   Colon,
   Equals,
   SemiColon,
+  LF,
+  CRLF
 }
 
 const lexer = buildLexer([
-  [true, /^\s+/g, TokenKind.Space],
+  [true, /^[ ]+/g, TokenKind.Space],
   [true, /^\-/g, TokenKind.Dash],
   [true, /^\d+/g, TokenKind.Number], // 数字模式
   [true, /^true|false/g, TokenKind.Boolean], // 布尔值模式
@@ -22,12 +25,15 @@ const lexer = buildLexer([
   [true, /^\:/g, TokenKind.Colon], // 冒号模式
   [true, /^\=/g, TokenKind.Equals], // 等号模式
   [true, /^\;/g, TokenKind.SemiColon], // 分号模式
+  [true, /^\n/g, TokenKind.LF], // 等号模式
+  [true, /^\r\n/g, TokenKind.CRLF], // 分号模式
 ]);
 
 const CONTENT = rule();
 const ARG = rule();
 const ARGK = rule();
 const ARGV = rule();
+const ARGS = rule()
 const CTRL = rule();
 const COMMENT = rule();
 const LINE = rule();
@@ -58,14 +64,22 @@ ARGV.setPattern(
   alt(apply(tok(TokenKind.Boolean), applyBoolean), apply(tok(TokenKind.Number), applyNumber), apply(tok(TokenKind.Identifier), applyString))
 )
 
+function applyArgK(value) {
+  return { type: 'arg', key: value.text, value: true }
+}
+
+function applyArgKv(value) {
+  return { type: 'arg', key: value[0].text, value: value[2] };
+}
+
 ARG.setPattern(
   alt(
-    ARGK,
-    seq(ARGK, str('='), ARGV)
+    apply(ARGK, applyArgK),
+    apply(seq(ARGK, str('='), ARGV), applyArgKv)
   )
 )
 
-CONTENT.setPattern(tok(TokenKind.Identifier))
+
 
 ARG_START.setPattern(
   seq(
@@ -73,41 +87,76 @@ ARG_START.setPattern(
   )
 )
 
+ARGS.setPattern(
+  seq(ARG_START, list_sc(ARG, ARG_START))
+)
+
+function applyCommand(value) {
+  if (!value) return undefined
+  return { type: 'command', value: value.text, text: value.text }
+}
+function applyColon(value) {
+  return { type: 'colon' }
+}
+
+function applyContent(value) {
+  console.log(value);
+  const words = value;
+  const validWords = words.filter(e => e?.type === 'word')
+  const validValue = validWords.map(e=>e.value).reduce((prev, curr) => prev +' '+ curr, '')
+  return { type: 'content', value: validValue }
+}
+
+function applyWord(value) {
+  return { type: 'word', value: value.text }
+}
+
+CONTENT.setPattern(
+  list_sc(apply(tok(TokenKind.Identifier), applyWord), tok(TokenKind.Space)))
+
+function applyCtrl(value) {
+  const parts = flattenDeep(value).filter(e => e?.type);
+  return parts
+}
+
 CTRL.setPattern(
-  seq(
-    opt(tok(TokenKind.Identifier)),
-    tok(TokenKind.Colon),
-    opt(tok(TokenKind.Identifier)),
-    opt(list_sc(ARG, ARG_START)),
+  apply(seq(
+    apply(opt(apply(tok(TokenKind.Identifier), applyCommand)), applyCommand),
+    apply(tok(TokenKind.Colon), applyColon),
+    opt(apply(CONTENT, applyContent)),
+    opt(ARGS),
     opt(COMMENT)
-  )
+  ), applyCtrl)
 )
 
 COMMENT.setPattern(
   seq(tok(TokenKind.SemiColon), opt(CONTENT))
 )
 
+function applyLn(value) {
+  const parts = flattenDeep(value).filter(e => e?.type);
+  if (!parts.find(e => e.type === 'command')) return undefined
+  return { type: 'line', value: parts }
+}
 
 LINE.setPattern(
-  alt(
+  apply(alt(
     COMMENT, CTRL, CONTENT
-  )
+  ), applyLn)
 )
 
+function applySctipt(value) {
+  return value.filter(e => e?.type === 'line');
+}
+
 SCRIPT.setPattern(
-  rep(
-    alt(
-      LINE,
-      seq(LINE, str("\n")),
-      seq(LINE, str("\r\n"))
-    )
-  )
+  apply(list_sc(LINE, alt(tok(TokenKind.LF), tok(TokenKind.CRLF))), applySctipt)
 );
 
-
-
-const script = `command:content contentaaa -arg=1 -argt=2 -argthree=true;comment`
+const script = `WebGAL:Hello Im WebGALs next generation parser -arg1=1 -arg2=2 -arg3=true -arg4;comment
+command:content1 -arg1=1 -arg2=2 -arg3=true -arg4;comment
+command:content2 -arg1=1 -arg2=2 -arg3=true -arg4;comment`
 
 const result = expectEOF(SCRIPT.parse(lexer.parse(script)))
-
 console.log(result);
+
