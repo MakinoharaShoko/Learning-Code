232d4e4d4af03f9e651c0e2f6d842d7dfc377fc8
Mahiru <Mahiru_@outlook.com>
2023-09-02 00:01:34 +0800

    ok!

--- React/playground/src/App.tsx
+++ React/playground/src/App.tsx
@@ -3,14 +3,18 @@ import './App.css'
 function Comp1() {
   const value1 = uVK(1, 'global1')
 
+  console.log('render11');
+  
   return <div onClick={() => { value1.v = value1.v + 1 }}>
     {value1.v} Click to +1
   </div>
 }
 
 function Comp2() {
-  const value2 = uVK(2, 'global1')
+  const value2 = uVK(1, 'global1')
 
+  console.log('render22');
+  
   return <div onClick={() => { value2.v = value2.v + 1 }}>
     {value2.v} Click to +1
   </div>
