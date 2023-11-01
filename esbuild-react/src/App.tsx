223e96b2dba5dff2d9968d2cf3bd3b04e4730933
Mahiru <mahiru_@outlook.com>
2023-11-01 11:02:06 +0800

    use js to pack

--- esbuild-react/src/App.tsx
+++ esbuild-react/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import s from './app.module.scss';
+import Counter from '@/components/Counter';
 
 export default function App() {
   return (
     <main>
       <p className={s.blue}>Hello World!</p>
       <p>This is a React Esbuild template!</p>
+      <Counter />
     </main>
   );
 }
---
+++ esbuild-react/src/components/Counter.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+
+export default function Counter() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div
+      onClick={() => {
+        setCount((count) => count + 1);
+      }}
+    >
+      Value is {count},Click to +1;
+    </div>
+  );
+}
