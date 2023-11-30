5df5c86966e90fab6faf0c5bf68a3f83399f59b4
Mahiru <Mahiru_@outlook.com>
2023-11-30 23:05:28 +0800

    Create Matcher.ts

---
+++ tools/Matcher.ts
@@ -0,0 +1,34 @@
+class Matcher<T, R = any> {
+    private subject: T;
+    private result: R | undefined;
+    private isEnd = false;
+  
+    constructor(subject: T) {
+      this.subject = subject;
+    }
+  
+    public with(pattern: T, fn: () => R): this {
+      if (!this.isEnd && this.subject === pattern) {
+        this.result = fn();
+        this.isEnd = true;
+      }
+      return this;
+    }
+  
+    public endsWith(pattern: T, fn: () => R) {
+      if (!this.isEnd && this.subject === pattern) {
+        this.result = fn();
+        this.isEnd = true;
+      }
+      return this.evaluate();
+    }
+  
+    public default(fn: () => R) {
+      if (!this.isEnd) this.result = fn();
+      return this.evaluate();
+    }
+  
+    private evaluate(): R | undefined {
+      return this.result;
+    }
+  }
